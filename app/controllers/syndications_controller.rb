b86ea655e1997f76d55f1f69fe24745384a34746
Peregrin Marshall <peremarshall@gmail.com>
2018-12-16 11:13:36 -0700

    adding class methods to find most recent feed, updating controller to new use class methods, adding mime-type for the txt extension

--- app/controllers/syndications_controller.rb
+++ app/controllers/syndications_controller.rb
@@ -2,10 +2,16 @@ class SyndicationsController < ApplicationController
 
 
   def tour_urls
-    @urls = Syndication.tour_urls
+    feed = Syndication.most_recent_urls.body
+    respond_to do |format|
+      format.txt { render plain: feed }
+    end
   end
 
   def tour_slides_descriptions
-    @slide_descriptions = Syndication.tour_slides_descriptions
+    feed = Syndication.most_recent_slide_descriptions.body
+    respond_to do |format|
+      format.txt { render plain: feed }
+    end
   end
 end
--- app/models/syndication.rb
+++ app/models/syndication.rb
@@ -2,4 +2,12 @@ class Syndication < ActiveRecord::Base
 
   scope :tour_urls, -> { where("name = ?", "tour_urls.txt") } 
   scope :tour_slides_descriptions, -> { where("name = ?", "tour_slides_descriptions.txt") } 
+
+  def self.most_recent_urls
+    Syndication.tour_urls.order("created_at desc, id desc").first
+  end
+
+  def self.most_recent_slide_descriptions
+    Syndication.tour_slides_descriptions.order("created_at desc, id desc").first
+  end
 end
