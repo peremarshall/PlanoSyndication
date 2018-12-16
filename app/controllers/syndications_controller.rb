c7a1f5521c293b0956bdcb2655ebc1889bdf3a1d
Peregrin Marshall <peremarshall@gmail.com>
2018-12-15 17:07:36 -0700

    adding initial queries to syndications controller

--- app/controllers/syndications_controller.rb
+++ app/controllers/syndications_controller.rb
@@ -2,8 +2,10 @@ class SyndicationsController < ApplicationController
 
 
   def tour_urls
+    @urls = Syndication.tour_urls
   end
 
   def tour_slides_descriptions
+    @slide_descriptions = Syndication.tour_slides_descriptions
   end
 end
