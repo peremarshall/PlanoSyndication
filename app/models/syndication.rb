7a75f6bc421e44a10789c3119827bec47a19c509
Peregrin Marshall <peremarshall@gmail.com>
2018-12-15 17:02:21 -0700

    adding scopes to build separate syndications

--- app/models/syndication.rb
+++ app/models/syndication.rb
@@ -1,3 +1,5 @@
 class Syndication < ActiveRecord::Base
 
+  scope :tour_urls, -> { where("name = ?", "tour_urls.txt") } 
+  scope :tour_slides_descriptions, -> { where("name = ?", "tour_slides_descriptions.txt") } 
 end
